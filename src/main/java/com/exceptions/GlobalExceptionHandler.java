a5ce74c2f0c8ee041d564bd78795bb39107370c8
n-ndini7 <hp@RJs-PC>
2020-12-11 09:51:14 +0530

    [Nandini] Added -SQLException , NoSuchElement and Exceptions to global exception handler

--- src/main/java/com/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.exceptions;
 
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,4 +24,23 @@ public class GlobalExceptionHandler {
 		ResponseDTO status = new ResponseDTO(e.getMessage());
 		return new ResponseEntity<ResponseDTO>(status, HttpStatus.OK);
 	}
+	@ExceptionHandler(SQLException.class)
+	public final ResponseEntity<ResponseDTO> QueryException(SQLException e) {
+		ResponseDTO status = new ResponseDTO("SQL exception thrown!!");
+		return new ResponseEntity<ResponseDTO>(status, HttpStatus.OK);
+	}
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	public final ResponseEntity<ResponseDTO> NoElementException(NoSuchElementException e) {
+		ResponseDTO status = new ResponseDTO("No such element exists!!");
+		return new ResponseEntity<ResponseDTO>(status, HttpStatus.OK);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ResponseDTO> AnonymousException(Exception e) {
+		ResponseDTO status = new ResponseDTO("Exception thrown!! \nCan't process your request.");
+		return new ResponseEntity<ResponseDTO>(status, HttpStatus.OK);
+	}
+	
+	
 }
